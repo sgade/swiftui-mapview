43fb016bf4f36f2c679ee97e8d047b86443a2088
Sören Gade <gade.soeren@gmail.com>
2020-06-19 14:14:33 +0200

    Remove obsolete tests.

--- Tests/SwiftUIMapViewTests/SwiftUIMapViewTests.swift
+++ Tests/SwiftUIMapViewTests/SwiftUIMapViewTests.swift
@@ -1,15 +1,6 @@
 import XCTest
-@testable import swiftui_mapview
+@testable import SwiftUIMapView
 
-final class swiftui_mapviewTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(swiftui_mapview().text, "Hello, World!")
-    }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+final class SwiftUIMapViewTests: XCTestCase {
+    static var allTests = []
 }
--- Tests/SwiftUIMapViewTests/XCTestManifests.swift
+++ Tests/SwiftUIMapViewTests/XCTestManifests.swift
@@ -3,7 +3,7 @@ import XCTest
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(swiftui_mapviewTests.allTests),
+        testCase(SwiftUIMapViewTests.allTests),
     ]
 }
 #endif
